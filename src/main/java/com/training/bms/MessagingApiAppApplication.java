59312c9b1617cc99f03fdf2d835281144a93af79
DEVI S <devs@dhcp-10-120-153-88.wifi.oracle.com>
2024-07-12 15:11:33 +0530

    changed for name

--- src/main/java/com/training/bms/MessagingApiAppApplication.java
+++ src/main/java/com/training/bms/MessagingApiAppApplication.java
@@ -2,18 +2,24 @@ package com.training.bms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MessagingApiAppApplication {
+	
+	@GetMapping("enjoyweekend")
+	public String printGreetings() {
+		return "HI DEVI";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingApiAppApplication.class, args);
 		System.out.println("Welcome to Message API 5");
-		printGreetings();
+	
 		
 	}
-	public static void printGreetings() {
-		System.out.println("Hi Oracle !!!!");
-	}
+	
 
 }
